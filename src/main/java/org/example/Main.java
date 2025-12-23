a3f980bb6ed41e7c9a0720784a13b2e94b420e53
coder-5 <vihaan.agrawalcoder@gmail.com>
2025-12-23 15:00:53 -0500

    Added new data structures for managing `Titles` and `Inventory` (`ArrayList`) in `Main.java`. Expanded `main` method to include instances of `Random` and `Scanner` for future input handling, and integrated invocation of `AddRanks` functions (`AddCultivationRanks` and `AddMartialRanks`).

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,18 @@ public class Main {
     public static HashMap<String, Integer> Stats = new HashMap<>();
     public static ArrayList<String> CultivationRanks = new ArrayList<>();
     public static ArrayList<String> MartialRanks = new ArrayList<>();
+    public static ArrayList<String> Titles = new ArrayList<>();
+    public static ArrayList<String> Inventory = new ArrayList<>();
 
     public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        AddRanks.AddCultivationRanks();
+        AddRanks.AddMartialRanks();
+
+
 
     }
 }
